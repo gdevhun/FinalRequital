129e6e57535d2d1308ef2c3a43169289890b9b67
gdevhun <wjh9330@naver.com>
2025-06-04 17:52:43 +0900

    Feat: TrapBase

---
+++ Source/FinalRequital/Actor/FRTrapBase.cpp
@@ -0,0 +1,98 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/FRTrapBase.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
+AFRTrapBase::AFRTrapBase()
+{
+    TrapMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TrapMesh"));
+    RootComponent = TrapMesh;
+
+    ASC = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+
+    CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+    CollisionBox->SetupAttachment(TrapMesh);
+    CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    CollisionBox->SetCollisionResponseToAllChannels(ECR_Overlap);
+    CollisionBox->SetGenerateOverlapEvents(true);
+
+    CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AFRTrapBase::OnTrapTriggered);
+    CollisionBox->OnComponentEndOverlap.AddDynamic(this, &AFRTrapBase::OnTrapEndOverlap);
+}
+
+void AFRTrapBase::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (ASC)
+    {
+        ASC->InitAbilityActorInfo(this, this);
+    }
+    GetWorldTimerManager().SetTimer(DamageTickTimer, this, &AFRTrapBase::DealDamageToOverlappingActors, DamageInterval, true);
+}
+
+class UAbilitySystemComponent* AFRTrapBase::GetAbilitySystemComponent() const
+{
+    return ASC;
+}
+
+void AFRTrapBase::OnTrapTriggered(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (!OtherActor || OtherActor == this) return;
+    OverlappingActors.Add(OtherActor);
+}
+
+void AFRTrapBase::OnTrapEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    OverlappingActors.Remove(OtherActor);
+}
+
+void AFRTrapBase::DealDamageToOverlappingActors()
+{
+    if (!bIsTrapActive || !ASC || !DamageEffectClass) return;
+
+    for (AActor* Target : OverlappingActors)
+    {
+        if (IsValid(Target))
+        {
+            ApplyTrapEffectToActor(Target);
+        }
+    }
+}
+
+void AFRTrapBase::ApplyTrapEffectToActor(AActor* TargetActor)
+{
+    if (!DamageEffectClass || !TargetActor || !ASC) return;
+
+    ActivateTrap();
+
+    UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+    if (!TargetASC) return;
+
+    FGameplayEffectContextHandle EffectContext = ASC->MakeEffectContext();
+    EffectContext.AddSourceObject(this);
+
+    FGameplayEffectSpecHandle SpecHandle = ASC->MakeOutgoingSpec(DamageEffectClass, 1.f, EffectContext);
+    if (SpecHandle.IsValid())
+    {
+        ASC->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), TargetASC);
+    }
+}
+
+void AFRTrapBase::ActivateTrap_Implementation()
+{
+    bIsTrapActive = true;
+    // 여기서 애니메이션, VFX, SFX 등을 활성화할 수 있음
+}
+
+void AFRTrapBase::DeactivateTrap_Implementation()
+{
+    bIsTrapActive = false;
+    // 여기서 애니메이션, VFX, SFX 등을 비활성화할 수 있음
+}
---
+++ Source/FinalRequital/Actor/FRTrapBase.h
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Actor.h"
+#include "FRTrapBase.generated.h"
+
+
+UCLASS()
+class FINALREQUITAL_API AFRTrapBase : public AActor, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	AFRTrapBase();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	TObjectPtr<class UAbilitySystemComponent> ASC;
+
+	UPROPERTY(EditAnywhere, Category = "Trap")
+	TObjectPtr<class UStaticMeshComponent> TrapMesh;
+
+	UPROPERTY(EditAnywhere, Category = "Trap")
+	TObjectPtr<class UBoxComponent> CollisionBox;
+
+	UPROPERTY(EditAnywhere, Category = "Trap")
+	TSubclassOf<class UGameplayEffect> DamageEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsTrapActive = true;
+
+	UPROPERTY(EditAnywhere, Category = "Trap")
+	float DamageInterval = 1.0f;
+
+	UFUNCTION()
+	virtual void OnTrapTriggered(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnTrapEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	void ApplyTrapEffectToActor(AActor* TargetActor);
+	void DealDamageToOverlappingActors();
+
+	FTimerHandle DamageTickTimer;
+	TSet<AActor*> OverlappingActors;
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void ActivateTrap();
+	
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void DeactivateTrap();
+
+};
