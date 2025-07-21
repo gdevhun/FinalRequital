b03aa3d25149167d4f036461de53890edcac24f8
gdevhun <wjh9330@naver.com>
2025-07-21 23:13:44 +0900

    Fix: Skill Weapon Gauge Size

---
+++ Source/FinalRequital/UI/HUD/FRPlayerHpBarWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/FRUserWidget.h"
+#include "FRPlayerHpBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALREQUITAL_API UFRPlayerHpBarWidget : public UFRUserWidget
+{
+	GENERATED_BODY()
+	
+};
