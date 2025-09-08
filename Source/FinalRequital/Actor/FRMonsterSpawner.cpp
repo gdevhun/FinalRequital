dfd72f0b699104ef69065bb0e7e589ffa7d9e258
gdevhun <wjh9330@naver.com>
2025-09-08 16:40:23 +0900

    Feat: MonsterSpawnManager

---
+++ Source/FinalRequital/Actor/FRMonsterSpawner.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/FRMonsterSpawner.h"
+
+#include "Character/FRMonsterBase.h"
+#include "Kismet/KismetMathLibrary.h"
+
+AFRMonsterSpawner::AFRMonsterSpawner()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	SpawnInterval = 5.0f;
+	SpecialSpawnPercentage = 10.0f; 
+}
+
+void AFRMonsterSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+}
+void AFRMonsterSpawner::StartSpawn()
+{
+	if (GetWorld())
+	{
+		GetWorld()->GetTimerManager().SetTimer(
+			SpawnTimerHandle,
+			this,
+			&AFRMonsterSpawner::SpawnMonster,
+			SpawnInterval,
+			true 
+		);
+	}
+}
+
+void AFRMonsterSpawner::StopSpawn()
+{
+	if (GetWorld())
+	{
+		GetWorld()->GetTimerManager().ClearTimer(SpawnTimerHandle);
+	}
+}
+
+void AFRMonsterSpawner::SpawnMonster()
+{
+	if (!GetWorld()) return;
+
+	TSubclassOf<AFRMonsterBase> MonsterToSpawn = nullptr;
+
+	const float RandomValue = UKismetMathLibrary::RandomFloatInRange(0.f, 100.f);
+
+	if (RandomValue <= SpecialSpawnPercentage)
+	{
+		const int32 RandomIndex = UKismetMathLibrary::RandomIntegerInRange(0, 2);
+		switch (RandomIndex)
+		{
+		case 0: MonsterToSpawn = DamageUpMonsterClass; break;
+		case 1: MonsterToSpawn = HpUpMonsterClass; break;
+		case 2: MonsterToSpawn = SpeedUpMonsterClass; break;
+		}
+	}
+	else
+	{
+		MonsterToSpawn = BaseMonsterClass;
+	}
+
+	if (MonsterToSpawn)
+	{
+		FActorSpawnParameters SpawnParams;
+		GetWorld()->SpawnActor<AFRMonsterBase>(
+			MonsterToSpawn,
+			GetActorLocation(),
+			GetActorRotation(),
+			SpawnParams
+		);
+	}
+}
+
---
+++ Source/FinalRequital/Actor/FRMonsterSpawner.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FRMonsterSpawner.generated.h"
+
+UCLASS()
+class FINALREQUITAL_API AFRMonsterSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AFRMonsterSpawner();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	TSubclassOf<class AFRMonsterBase> BaseMonsterClass;
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	TSubclassOf<class AFRMonsterBase> DamageUpMonsterClass;
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	TSubclassOf<class AFRMonsterBase> HpUpMonsterClass;
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	TSubclassOf<class AFRMonsterBase> SpeedUpMonsterClass;
+
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	float SpawnInterval;
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	float SpecialSpawnPercentage;
+
+	UFUNCTION(BlueprintCallable)
+	void SpawnMonster();
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void StartSpawn();
+	UFUNCTION(BlueprintCallable)
+	void StopSpawn();
+
+private:
+	FTimerHandle SpawnTimerHandle;
+
+};
