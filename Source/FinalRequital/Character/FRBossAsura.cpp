26040d1a77ba2c4dca4e0b4d2dd161a5f34f0b6f
gdevhun <wjh9330@naver.com>
2025-10-17 00:54:48 +0900

    Feat: BossBasic Class

---
+++ Source/FinalRequital/Character/FRBossAsura.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/FRBossAsura.h"
+
+AFRBossAsura::AFRBossAsura()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AFRBossAsura::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
